ad020c6d27193c1cef0e0210be0316517d21e554
lazar <eleazartodor@gmail.com>
2022-06-21 11:19:47 +0200

    Adding fetch() method

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,6 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+fetch("data/photographers.json")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
