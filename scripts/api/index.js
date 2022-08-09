4e1a88dd92dede4bb546be2986d4483cd822a946
Lazar Todorovic <eleazartodor@gmail.com>
2022-08-09 17:25:21 +0200

    Adding api folder

---
+++ scripts/api/index.js
@@ -0,0 +1,24 @@
+async function getPhotographers() {
+  const data = fetch("data/photographers.json")
+    .then((res) => res.json())
+    .then((data) => data);
+  return data;
+}
+
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
