325020e6f289f3b7bb2173324d656e3c10811d9b
Lazar Todorovic <eleazartodor@gmail.com>
2022-08-16 17:54:57 +0200

    adding new files

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,21 @@
-//Mettre le code JavaScript lié à la page photographer.html
+import {getPhotographerById} from "../api/media.js"
+import {photographerMediaFactory} from "../factories/photographerMediaFactory.js"
+
+
+async function displayData(medias) {
+    const photographersSection = document.querySelector(".photograph-header");
+    medias.forEach((media) => {
+      const photographerModel = photographerMediaFactory(media);
+      const userCardDOM = photographerModel.getUserMedia();
+      photographersSection.appendChild(userCardDOM);
+    });
+  }
+  
+  async function init() {
+    // Récupère les datas des photographes
+    const { medias } = await getPhotographerById();
+    displayData(medias);
+  }
+  
+  init();
+
