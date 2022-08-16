4164d4ef1bb6fac95ff163799e6582e4cab02683
Lazar Todorovic <eleazartodor@gmail.com>
2022-08-16 22:13:00 +0200

    photographerGalleryFactory

---
+++ scripts/factories/photographerGalleryFactory.js
@@ -0,0 +1,22 @@
+import { getPhotographerById } from "../api/media.js";
+
+export function photographerGalleryFactory(data) {
+
+const {title,image,price,date,likes,name} = data;
+
+    function getUserGallery(){
+        const galleryDiv = document.createElement("div");
+        galleryDiv.classList.add("gallery-images");
+        
+        const picture = `assets/photographers_photos/${name}/${image}`;
+        const img = document.createElement( "img" );
+        img.classList.add("galleryPics");
+        img.setAttribute("alt", "photo" +" " + title);
+        img.setAttribute("src",picture);
+        
+        galleryDiv.append(img);
+        
+        return galleryDiv;
+    }
+return ({getUserGallery});
+}
