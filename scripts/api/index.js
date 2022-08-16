c433965c1e2f10ac0013ebc3b542ebe6cb565075
Lazar Todorovic <eleazartodor@gmail.com>
2022-08-16 17:55:22 +0200

    refacturing folders

--- scripts/api/index.js
+++ scripts/api/index.js
@@ -1,24 +1,7 @@
-async function getPhotographers() {
+export async function getPhotographers() {
   const data = fetch("data/photographers.json")
     .then((res) => res.json())
     .then((data) => data);
+    console.log(data);
   return data;
 }
-
-async function displayData(photographers) {
-  const photographersSection = document.querySelector(".photographer_section");
-
-  photographers.forEach((photographer) => {
-    const photographerModel = photographerFactory(photographer);
-    const userCardDOM = photographerModel.getUserCardDOM();
-    photographersSection.appendChild(userCardDOM);
-  });
-}
-
-async function init() {
-  // Récupère les datas des photographes
-  const { photographers } = await getPhotographers();
-  displayData(photographers);
-}
-
-init();
