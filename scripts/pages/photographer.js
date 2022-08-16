898d9b2ef3772c88a777f732a8973a1fae1ced8e
Lazar Todorovic <eleazartodor@gmail.com>
2022-08-16 22:12:49 +0200

    photographerGallery import

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,16 +1,21 @@
 import {getPhotographerById} from "../api/media.js"
 import {photographerMediaFactory} from "../factories/photographerMediaFactory.js"
+import {photographerGalleryFactory} from "../factories/photographerGalleryFactory.js"
 
 const url = new URL(window.location.href);
 const photographerId = url.searchParams.get("id");
 
 async function displayData(photographers) {
     const photographersSection = document.querySelector(".photograph-header");
+    const photographerGallery = document.querySelector(".photographer-gallery");
     photographers.forEach((photographer) => {
       if (photographer.id == photographerId){
       const photographerModel = photographerMediaFactory(photographer);
+      const photographerGalleryImage = photographerGalleryFactory(photographer);
+      const userGallery = photographerGalleryImage.getUserGallery();
       const userCardDOM = photographerModel.getUserMedia();
       const userPhotoDOM = photographerModel.getUserPhoto();
+      photographerGallery.appendChild(userGallery);
       photographersSection.appendChild(userCardDOM);
       photographersSection.appendChild(userPhotoDOM);
       }
