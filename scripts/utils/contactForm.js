b5922232e647952445651ce6dc00b85e30bac6e0
Lazar Todorovic <lazar.todorovic@jack.agency>
2022-08-23 16:25:48 +0200

    Form validation

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,21 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+const nom = document.getElementById('nom');
+const email = document.getElementById('email');
+const prenom = document.getElementById('prenom');
+const textarea = document.getElementById('textarea');
+
+const form = document.querySelector('#form');
+ 
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
+    e.preventDefault();
+    console.log("Votre validation :",nom.value,
+    prenom.value,
+    email.value,
+    textarea.value);
+    closeModal();
+
+});
