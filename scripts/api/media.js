fff2a13e93d1b345ce5349e88eb92eb3760f2555
Lazar Todorovic <lazar.todorovic@jack.agency>
2022-08-23 13:46:10 +0200

    deleted file

--- scripts/api/media.js
+++
@@ -1,7 +0,0 @@
-export async function getPhotographerById() {
-  const data = fetch("data/photographers.json")
-    .then((res) => res.json())
-    .then((data) => data);
-    console.log(data);
-  return data;
-}
