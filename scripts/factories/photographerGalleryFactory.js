86dc6d4ccf8e5758985429564990358b51bfde40
Lazar Todorovic <lazar.todorovic@jack.agency>
2022-08-23 15:24:31 +0200

    layout image gallery

--- scripts/factories/photographerGalleryFactory.js
+++ scripts/factories/photographerGalleryFactory.js
@@ -6,13 +6,43 @@ export function photographerGalleryFactory(medias, path) {
         galleryDiv.classList.add("gallery-images")
         
         medias.forEach(media => {
-            const {image, title} = media
+            const {image, title, likes} = media
+
+            const container = document.createElement("div")
+            container.classList.add("container")
+
+            const imageDiv = document.createElement("div")
+            imageDiv.classList.add("image-container")
+
+            const textDiv = document.createElement("div")
+            textDiv.classList.add("text-container")
+
             const picture = `${path}/${image}`
+
             const img = document.createElement( "img" )
             img.classList.add("galleryPics")
             img.setAttribute("alt", title)
             img.setAttribute("src",picture)
-            galleryDiv.append(img)
+            img.setAttribute("tabindex", 0)
+
+            const titleText = document.createElement( "h4" )
+            titleText.classList.add("imageTitle")
+            titleText.innerText = title
+
+            const like = document.createElement( "p" )
+            const heart = document.createElement( "span" )
+            heart.classList.add("heart")
+            heart.innerHTML = `<i class="fa-solid fa-heart"></i>`
+            like.classList.add("imageLike")
+            like.textContent=likes
+
+            imageDiv.appendChild(img)
+            textDiv.appendChild(titleText)
+            textDiv.appendChild(like)
+            textDiv.appendChild(heart)
+            container.append(imageDiv)
+            container.append(textDiv)
+            galleryDiv.appendChild(container)
         })
         return galleryDiv
     }
