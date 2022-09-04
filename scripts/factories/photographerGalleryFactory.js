dcc5246ecaea86fc940d5034c42bf98c25e2a6b9
Lazar Todorovic <eleazartodor@gmail.com>
2022-09-04 23:09:38 +0200

    adding video to factory method

--- scripts/factories/photographerGalleryFactory.js
+++ scripts/factories/photographerGalleryFactory.js
@@ -4,10 +4,56 @@ export function photographerGalleryFactory(medias, path) {
     function getUserGallery(){
         const galleryDiv = document.createElement("div")
         galleryDiv.classList.add("gallery-images")
+
+        
         
         medias.forEach(media => {
-            const {image, title, likes} = media
+        const {image, title, likes, video} = media
 
+            if(media.video){
+            const videoDiv = document.createElement("div")
+            videoDiv.classList.add("container")
+
+            const videoSection = document.createElement( "div")
+            videoSection.classList.add("video-container")
+
+            const textDiv = document.createElement("div")
+            textDiv.classList.add("text-container")
+
+            const videoData = `${path}/${video}`
+
+            const mp4 = document.createElement( "video" )
+            mp4.classList.add("galleryPics")
+            mp4.setAttribute("controls","controls")
+            mp4.setAttribute("alt", title)
+            mp4.setAttribute("src",videoData)
+            mp4.setAttribute("tabindex", 0)
+            mp4.setAttribute("type","video/mp4")
+
+
+            const titleText = document.createElement( "h4" )
+            titleText.classList.add("videoTitle")
+            titleText.innerText = title
+
+            const like = document.createElement( "p" )
+            const heart = document.createElement( "span" )
+            heart.classList.add("heart")
+            heart.innerHTML = `<i class="fa-solid fa-heart"></i>`
+            like.classList.add("imageLike")
+            like.textContent=likes
+
+            videoDiv.appendChild(mp4)
+            textDiv.appendChild(titleText)
+            textDiv.appendChild(like)
+            textDiv.appendChild(heart)
+
+            videoDiv.append(videoSection)
+            videoDiv.append(textDiv)
+            galleryDiv.appendChild(videoDiv)
+
+            }
+            
+            if(media.image){
             const container = document.createElement("div")
             container.classList.add("container")
 
@@ -26,6 +72,7 @@ export function photographerGalleryFactory(medias, path) {
             img.setAttribute("src",picture)
             img.setAttribute("tabindex", 0)
 
+
             const titleText = document.createElement( "h4" )
             titleText.classList.add("imageTitle")
             titleText.innerText = title
@@ -43,7 +90,9 @@ export function photographerGalleryFactory(medias, path) {
             textDiv.appendChild(heart)
             container.append(imageDiv)
             container.append(textDiv)
-            galleryDiv.appendChild(container)
+            galleryDiv.appendChild(container)}
+
+            
         })
         return galleryDiv
     }
@@ -51,4 +100,3 @@ export function photographerGalleryFactory(medias, path) {
 return ({getUserGallery})
 }
 
-//! make the gallery today
