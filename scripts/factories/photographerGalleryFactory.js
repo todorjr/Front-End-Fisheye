2c58a934a1d445ae0eeb7eb4ebdf2b7eacb1f24b
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2022-09-05 17:37:24 +0200

    refactoring code with factory pattern

--- scripts/factories/photographerGalleryFactory.js
+++ scripts/factories/photographerGalleryFactory.js
@@ -1,27 +1,101 @@
-export function photographerGalleryFactory(medias, path) {
-
+/**
+ * Class to share common methods between media type.
+ */
+// class BaseGalleryElement {
+//     #media
     
+//     constructor(media) {
+//         this.#media = media;
+//     }
+
+//     get title () {
+//         return this.#media.title;
+//     }
+
+//     toElement () {
+//         throw new Error('NOT IMPLEMENTED');
+//     }
+// }
+
+/**
+ * Generate a video element.
+ */
+// class VideoGalleryElement extends BaseGalleryElement {
+//     toElement () {
+//         const element = document.createElement('div');
+
+//         element.classList.add('video');
+        
+//         return element
+//     }
+// }
+
+/**
+ * Generate a image element.
+ */
+// class ImageGalleryElement extends BaseGalleryElement {
+//     toElement () {
+//         const element = document.createElement('div');
+
+//         element.classList.add('image');
+        
+//         return element
+//     }
+// }
+
+/**
+ * Returns element instance based on media type.
+ */
+// function getGalleryElementFromMedia (media) {
+//     if (media.image !== undefined) {
+//         return new ImageGalleryElement(media)
+//     }
+    
+//     if (media.video !== undefined) {
+//         return new VideoGalleryElement(media)
+//     }
+
+//     throw new Error('UNKNOWN MEDIA TYPE');
+// }
+
+export function photographerGalleryFactory(medias, path) {
+    // function getUserGalleryUsingClasses () {
+    //     const galleryDiv = document.createElement("div")
+        
+    //     medias.forEach((media) => {
+    //         let item = getGalleryElementFromMedia(media)
+    //         const element = item.toElement()
+
+    //         galleryDiv.appendChild(element)
+    //     })
+
+        
+    //     galleryDiv.classList.add("gallery-images")
+
+    //     return galleryDiv
+    // }
+
     function getUserGallery(){
         const galleryDiv = document.createElement("div")
         galleryDiv.classList.add("gallery-images")
-
+    
         
         
         medias.forEach(media => {
         const {image, title, likes, video} = media
-
+    
             if(media.video){
             const videoDiv = document.createElement("div")
             videoDiv.classList.add("container")
-
+    
             const videoSection = document.createElement( "div")
             videoSection.classList.add("video-container")
-
+    
             const textDiv = document.createElement("div")
             textDiv.classList.add("text-container")
-
+    
             const videoData = `${path}/${video}`
-
+    
             const mp4 = document.createElement( "video" )
             mp4.classList.add("galleryPics")
             mp4.setAttribute("controls","controls")
@@ -29,13 +103,13 @@ export function photographerGalleryFactory(medias, path) {
             mp4.setAttribute("src",videoData)
             mp4.setAttribute("tabindex", 0)
             mp4.setAttribute("type","video/mp4")
-
-
+    
+    
             const titleText = document.createElement( "h4" )
             titleText.classList.add("videoTitle")
             titleText.setAttribute("tabindex", 0)
             titleText.innerText = title
-
+    
             const like = document.createElement( "p" )
             const heart = document.createElement( "span" )
             heart.setAttribute("tabindex", 0)
@@ -43,42 +117,42 @@ export function photographerGalleryFactory(medias, path) {
             heart.innerHTML = `<i class="fa-solid fa-heart"></i>`
             like.classList.add("imageLike")
             like.textContent=likes
-
+    
             videoDiv.appendChild(mp4)
             textDiv.appendChild(titleText)
             textDiv.appendChild(like)
             textDiv.appendChild(heart)
-
+    
             videoDiv.append(videoSection)
             videoDiv.append(textDiv)
             galleryDiv.appendChild(videoDiv)
-
+    
             }
             
             if(media.image){
             const container = document.createElement("div")
             container.classList.add("container")
-
+    
             const imageDiv = document.createElement("div")
             imageDiv.classList.add("image-container")
-
-
+    
+    
             const textDiv = document.createElement("div")
             textDiv.classList.add("text-container")
-
+    
             const picture = `${path}/${image}`
-
+    
             const img = document.createElement( "img" )
             img.classList.add("galleryPics")
             img.setAttribute("alt", title)
             img.setAttribute("src",picture)
             img.setAttribute("tabindex", 0)
-
-
+    
+    
             const titleText = document.createElement( "h4" )
             titleText.classList.add("imageTitle")
             titleText.innerText = title
-
+    
             const like = document.createElement( "p" )
             const heart = document.createElement( "span" )
             heart.setAttribute("tabindex", 0)
@@ -86,7 +160,7 @@ export function photographerGalleryFactory(medias, path) {
             heart.innerHTML = `<i class="fa-solid fa-heart"></i>`
             like.classList.add("imageLike");
             like.textContent=likes
-
+    
             imageDiv.appendChild(img)
             textDiv.appendChild(titleText)
             textDiv.appendChild(like)
@@ -94,12 +168,12 @@ export function photographerGalleryFactory(medias, path) {
             container.append(imageDiv)
             container.append(textDiv)
             galleryDiv.appendChild(container)}
-
+    
             
         })
         return galleryDiv
     }
 
-return ({getUserGallery})
+    return { getUserGallery }
 }
 
