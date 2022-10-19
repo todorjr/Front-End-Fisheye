562a8c9412b4f5957bf9995551dc6801aa32aa58
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2022-10-20 00:27:52 +0200

    refacturing

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,11 +1,11 @@
 import {getPhotographers} from "../api/index.js"
-import {photographerFactory} from "../factories/photographer.js"
+import ArtistFactory from "../factories/ArtistFactory.js"
 
 async function displayData(photographer) {
     const photographersSection = document.querySelector(".photographer_section");
   
     photographer.forEach((photographer) => {
-      const photographerModel = photographerFactory(photographer);
+      const photographerModel = new ArtistFactory(photographer);
       const userCardDOM = photographerModel.getUserCardDOM();
       photographersSection.appendChild(userCardDOM);
     });
