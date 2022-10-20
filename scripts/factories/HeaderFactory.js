7d9f98e54bfc689e80ed3bd1416d1469ac2c7cf5
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2022-10-20 14:53:05 +0200

    photographer header

---
+++ scripts/factories/HeaderFactory.js
@@ -0,0 +1,12 @@
+import PhotographerHeader from "../model/PhotographerHeader.js";
+
+
+export default class HeaderFactory{
+   constructor(data){
+    if (data.portrait !== undefined) {
+      return new PhotographerHeader(data)
+  }
+  
+  throw new Error('UNKNOWN MEDIA TYPE');
+   }
+}
---
+++ scripts/model/PhotographerHeader.js
@@ -0,0 +1,38 @@
+import  Photographer  from "../model/Photographer.js";
+
+export default class PhotographerHeader extends Photographer {
+    constructor(data){
+        super(data)
+        this.portrait= data.portrait
+    }
+    
+    toElement () {
+     
+        const picture = `assets/photographers/${this.portrait}`;
+        const article = document.createElement("article");
+        article.setAttribute("tabindex", "0");
+        article.classList.add("photographer-header__article");
+        const img = document.createElement("img");
+        img.setAttribute("alt", this.name);
+        img.setAttribute("title","Profil picture");
+        img.setAttribute("src", picture);
+        img.classList.add("profilPics");
+        
+        const h2 = document.createElement("h2");
+        const p = document.createElement("p");
+        const textContent = document.createElement("p");
+        textContent.textContent = this.tagline;
+        h2.textContent = this.name;
+        const div = document.createElement("div");
+        p.textContent = this.city + "," + " " + this.country;
+        p.style.color = "#d3573c";
+        p.style.fontSize = "12px";
+    
+        article.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(div);
+        div.appendChild(p);
+        div.appendChild(textContent);
+        return article;
+    }
+}
